2948fa3093e1e01d32c4f2e5ff9bde76b158ce82
Mingun Cho <mingun0108@gmail.com>
2022-08-09 19:04:21 -0700

    Fixed device order misdisplaying

--- main.js
+++ main.js
@@ -1,11 +1,4 @@
-const resultFiles = ["Pixel 6 (ARM Mali - G78)", "Samsung SM-T500 (Qualcomm Adreno 610)", "Qualcomm Lahaina (Qualcomm Adreno 660)", "Samsung SM-T510 (ARM Mali - G71)", "Motorola Moto G Pure (PowerVR GE8320)", "Nvidia Shield TV Pro (NVIDIA Tegra X1)"];
-
-const deviceSpecLink = ["https://www.notebookcheck.net/ARM-Mali-G78-MP20-GPU-Benchmarks-and-Specs.581804.0.html",
-                        "https://www.notebookcheck.net/Qualcomm-Adreno-610-GPU-Benchmarks-and-Specs.434054.0.html",
-                        "https://www.notebookcheck.net/Qualcomm-Adreno-660-GPU-Benchmarks-and-Specs.513908.0.html",
-                        "https://www.notebookcheck.net/ARM-Mali-G71-MP2-GPU.273124.0.html",
-                        "https://www.notebookcheck.net/PowerVR-GE8320-Graphics-Card-Benchmarks-and-Specs.372646.0.html",
-                        "https://www.notebookcheck.net/NVIDIA-Tegra-X1-SoC-for-Tablets-Processor-Specs-and-Benchmarks.137102.0.html"];
+const resultFiles = ["Samsung SM-T510 (ARM Mali - G71)", "Pixel 6 (ARM Mali - G78)", "Samsung SM-T500 (Qualcomm Adreno 610)", "Qualcomm Lahaina (Qualcomm Adreno 660)", "Motorola Moto G Pure (PowerVR GE8320)", "Nvidia Shield TV Pro (NVIDIA Tegra X1)"];
 
 const litmusTest = ["Message Passing Default", "Store Default", "Read Default",
                     "Load Buffer Default", "Store Buffer Default", "2+2 Write Default"];
@@ -17,9 +10,6 @@ const workgroupSize = [[16,32], [4,8], [4,16], [4,8], [8,16], [16,32]];
 const cellColor = ["#CCFFCC", "#CCFFFF", "#FFCCCC"];
 
 function main() {
-
-    var table = document.getElementById("table");
-    var testCount = 0;
 
     resultFiles.forEach(resultFile => {
         var fileName = "./results/" + resultFile + ".json";
@@ -58,7 +48,7 @@ function main() {
                     })
                 }
 
-                var tr = document.createElement('tr');
+                var tr = document.getElementById(resultFile);
                 var th = document.createElement('th');
 
                 // Add device column
@@ -67,21 +57,23 @@ function main() {
                 th.innerHTML = resultLink + resultFile + "</a>";
                 tr.appendChild(th);
 
+                var index = resultFiles.findIndex(name => name === resultFile);
+
                 // Add workgroup number column
-                var minWorkgroupNum = workgroupNum[testCount][0];
-                var maxWorkgroupNum = workgroupNum[testCount][1];
+                var minWorkgroupNum = workgroupNum[index][0];
+                var maxWorkgroupNum = workgroupNum[index][1];
                 var workgroupNumRange = document.createTextNode(minWorkgroupNum + "-" + maxWorkgroupNum);
                 th = document.createElement('th');
-                th.setAttribute('class', 'testColumn');
+                th.setAttribute('class', 'workgroupColumn');
                 th.appendChild(workgroupNumRange);
                 tr.appendChild(th);
 
                 // Add workgroup size column
-                var minWorkgroupSize = workgroupSize[testCount][0];
-                var maxWorkgroupSize = workgroupSize[testCount][1];
+                var minWorkgroupSize = workgroupSize[index][0];
+                var maxWorkgroupSize = workgroupSize[index][1];
                 var workgroupSizeRange = document.createTextNode(minWorkgroupSize + "-" + maxWorkgroupSize);
                 th = document.createElement('th');
-                th.setAttribute('class', 'testColumn');
+                th.setAttribute('class', 'workgroupColumn');
                 th.appendChild(workgroupSizeRange);
                 tr.appendChild(th);
 
@@ -131,19 +123,6 @@ function main() {
                     th.appendChild(nestedTable)
                     tr.appendChild(th);
                 })
-                table.appendChild(tr);
-
-                // Add device spec link
-                var deviceSpecs = document.getElementById('deviceSpecs');
-
-                var listElement = document.createElement('li');
-                var deviceLink =  "<a href=\"" + deviceSpecLink[testCount] + "\">";
-
-                listElement.innerHTML = deviceLink + resultFile + "</a>";
-                deviceSpecs.appendChild(listElement);
-
-
-                testCount++;
             })
             .catch((error) => {
                 console.error(error);
